448f753fcd33196a109058ace62c67330e53ec09
scodellarovincenzo2005-cpu <scodellarovincenzo2005@gmail.com>
2026-05-27 11:50:20 +0200

    modifica scodellaro

---
+++ src/main/java/model/Cliente.java
@@ -0,0 +1,16 @@
+package model;
+import model.*;
+/**
+ * The type Utente.
+ */
+public class Cliente {
+    private final String login;
+    private String password;
+    private int idCliente;
+    private String nome;
+    private String cognome;
+    private String patente;
+    private String email;
+
+
+}
